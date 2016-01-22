4949e06781e319ad97c911c0c4936433e4a8e09c
jcsizudev <takeshi.izu@gmail.com>
2016-01-22 17:54:17 +0900

    修正0122

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,14 +8,9 @@
  * Controller of the angsmpApp
  */
 angular.module('angsmpApp')
-  .controller('MainCtrl', [ '$scope', function ($scope) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', [ '$scope', 'uiGmapLogger', function ($scope, $log) {
+    $log.currentLevel = $log.LEVELS.debug;
 
-    $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
     angular.extend($scope, {
       map: {
         center: {
@@ -25,9 +20,28 @@ angular.module('angsmpApp')
         zoom: 8,
         events: {
           tilesloaded: function (map, eventName, originalEventArgs) {
-            console.log(map);
+            $log.debug(eventName);
+            $log.debug(originalEventArgs);
+          },
+          click: function (mapModel, eventName, originalEventArgs) {
+            $log.debug(eventName);
+            $log.debug(originalEventArgs[0]);
+            if ($scope.randomMarkers.length === 0) {
+              $log.debug('no marker.');
+              $scope.randomMarkers.push({
+                id: 'sample-1',
+                latitude: originalEventArgs[0].latLng.lat(),
+                longitude: originalEventArgs[0].latLng.lng(),
+                title: 'sample'
+              });
+              $scope.$evalAsync();
+            }
+            else {
+              $log.debug('marker exists.');
+            }
           }
         }
-      }
+      },
+      randomMarkers: []
     });
   }]);
