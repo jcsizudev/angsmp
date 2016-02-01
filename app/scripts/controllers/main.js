6fec817c835b7ad462fdc381425219c777174591
jcsizudev <takeshi.izu@gmail.com>
2016-02-01 18:08:34 +0900

    修正分0201

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,126 +9,190 @@
  */
 angular.module('angsmpApp')
   .controller('MainCtrl', [ '$scope', 'uiGmapLogger', 'RouteMarker', 'uiGmapGoogleMapApi', function ($scope, $log, RouteMarker, GoogleMapApi) {
-    $log.currentLevel = $log.LEVELS.debug;
-    $scope.test = "set";
+    $log.currentLevel = $log.LEVELS.debug;  // ログレベルの設定
+    $scope.test = 'set';  // テスト動作確認用
 
+    // google mapの準備完了待ち
     GoogleMapApi.then(function(maps) {
+      // スコープのセットアップ
       angular.extend($scope, {
+        // マップ操作用スコープ
         map: {
+          // 初期位置
           center: {
-            latitude: 45,
-            longitude: -73
+            latitude: 35.687487,
+            longitude: 139.764280
           },
-          zoom: 8,
+          // ズーム
+          zoom: 14,
+          // マップイベント定義
           events: {
+            // 地図画像ロード
             tilesloaded: function (map, eventName, orgEv) {
               $log.debug(eventName);
               $log.debug(orgEv);
             },
+            // 地図クリック
             click: function (mapModel, eventName, orgEv) {
               $log.debug('map:' + eventName);
               $log.debug(orgEv);
+
+              // クリック位置にマーカーを追加
               RouteMarker.addMarker(orgEv[0].latLng.lat(), orgEv[0].latLng.lng());
               $scope.$evalAsync();
-
-              $log.debug('center-');
-              $log.debug($scope.map.getGMap().getBounds().getNorthEast());
             }
           }
         },
+        // マーカー操作用スコープ
         markers: {
+          // ルートマーカーのマーカー配列をモデルとする。
           models: RouteMarker.getModel(),
+          // ドラッグ可能
           options: {
             draggable: true
           },
+          // イベント定義
           events: {
+            // ドラッグ開始
             dragstart: function (marker, eventName, args) {
               $log.debug('marker:' + eventName);
             },
+            // ドラッグ中
             drag: function (marker, eventName, args) {
               $log.debug('marker:' + eventName);
+              // ドラッグ位置でモデルのマーカーを更新
               var curMarker = RouteMarker.getMarker(marker.key);
               curMarker.latitude = marker.getPosition().lat();
-              curMarker.longitude = marker.getPosition().lng()
+              curMarker.longitude = marker.getPosition().lng();
             },
+            // ドラッグ終了
             dragend: function (marker, eventName, args) {
               $log.debug('marker:' + eventName);
             },
+            // マーカーをクリック
             click: function (marker, eventName, args) {
-              var sw = $scope.map.getGMap().getBounds().getSouthWest();
-              var ne = $scope.map.getGMap().getBounds().getNorthEast();
-              var cp = $scope.map.getGMap().getCenter();
-
               $log.debug('marker:' + eventName);
-              $log.debug($scope.markers.calcNewMarkerPos(sw, ne, marker.getPosition(), cp));
               $log.debug(marker);
 
+              // 対象マーカーを保存、バウンスアニメーションを設定
               $scope.markers.dialogTarget = marker;
-              marker.setAnimation(google.maps.Animation.BOUNCE);
+              marker.setAnimation(maps.Animation.BOUNCE);
 
-              $("#dlgLatitude").text(marker.getPosition().lat());
-              $("#dlgLongitude").text(marker.getPosition().lng());
-              $("#markerModal").modal();
+              // ダイアログの表示値（緯度経度）を設定して、ダイアログをモーダル表示
+              $('#dlgLatitude').text(marker.getPosition().lat());
+              $('#dlgLongitude').text(marker.getPosition().lng());
+              $('#markerModal').modal();
             }
           },
+          // ダイアログ選択中マーカー保存用
           dialogTarget: undefined,
+          // マーカー挿入時の位置情報作成
           calcNewMarkerPos: function (sw, ne, mkp, cp) {
-            var xdir, ydir;
-            var xlen, ylen;
+            var xdir;
+            var ydir;
+            var xlen;
+            var ylen;
             // マーカー配置方向決定
-            if (cp.lat() > mkp.lat())
-              ydir = 1.0;
-            else
+            if (cp.lat() > mkp.lat()) {
               ydir = -1.0;
-            if (cp.lng() > mkp.lng())
-              xdir = 1.0;
-            else
+            }
+            else {
+              ydir = 1.0;
+            }
+            if (cp.lng() > mkp.lng()) {
               xdir = -1.0;
+            }
+            else {
+              xdir = 1.0;
+            }
 
             // マーカー配置距離設定
-            ylen = (sw.lat() - ne.lat()) / 10.0;
-            xlen = (sw.lng() - ne.lng()) / 10.0;
+            ylen = (sw.lat() - ne.lat()) / 20.0;
+            xlen = (sw.lng() - ne.lng()) / 20.0;
 
+            // 緯度経度オブジェクトを作成
             var newMkp = {
               latitude: mkp.lat() + (ylen * ydir),
               longitude: mkp.lng() + (xlen * xdir)
             };
 
             return newMkp;
+          },
+          // マーカー挿入
+          insertNewMarker: function (marker, prevInsert) {
+            var posInfo = $scope.markers.calcNewMarkerPos(
+              $scope.map.getGMap().getBounds().getSouthWest(),
+              $scope.map.getGMap().getBounds().getNorthEast(),
+              marker.getPosition(),
+              $scope.map.getGMap().getCenter()
+            );
+            RouteMarker.insMarker(
+              marker.key,
+              posInfo.latitude,
+              posInfo.longitude,
+              prevInsert
+            );
           }
         },
+        // マーカーを結ぶ線分表示用スコープ
         polyline: {
+          // モデルをマーカー配列と共有する
           path: RouteMarker.getModel(),
+          // 線の描画情報
           stroke: {
             color: '#6060FB',
             weight: 3
           },
+          // マーカー操作で編集させるため、編集はオプションは不可を設定
           editable: false,
+          // マーカー操作で編集させるため、ドラッグオプションも不可を設定
           draggable: false,
           geodesic: true,
           visible: true,
           icons: [
             {
               icon: {
-                path: google.maps.CIRCLE
+                path: maps.CIRCLE
               }
             }
           ]
         }
       });
 
-      $("#dlgDelBtn").on('click', function (e) {
-        console.log('dlgDelBtn-click!');
+      // ダイアログ操作のイベント定義
+      // 削除ボタンイベント
+      $('#dlgDelBtn').on('click', function (e) {
+        $log.debug('dlgDelBtn-click!');
         if ($scope.markers.dialogTarget !== undefined) {
           RouteMarker.deleteMarker($scope.markers.dialogTarget.key);
-          $scope.markers.dialogTarget = undefined;
           $scope.$evalAsync();
-          $("#markerModal").modal('hide');
+          $('#markerModal').modal('hide');
         }
       });
 
-      $("#markerModal").on('hide.bs.modal', function (e) {
-        console.log( "Hide dialog." );
+      // マーカー挿入（前）イベント
+      $('#dlgInsPrev').on('click', function (e) {
+        $log.debug('dlgInsPrev-click!');
+        if ($scope.markers.dialogTarget !== undefined) {
+          $scope.markers.insertNewMarker($scope.markers.dialogTarget, true);
+          $scope.$evalAsync();
+          $('#markerModal').modal('hide');
+        }
+      });
+
+      // マーカー挿入（後）イベント
+      $('#dlgInsNext').on('click', function (e) {
+        $log.debug('dlgInsNext-click!');
+        if ($scope.markers.dialogTarget !== undefined) {
+          $scope.markers.insertNewMarker($scope.markers.dialogTarget, false);
+          $scope.$evalAsync();
+          $('#markerModal').modal('hide');
+        }
+      });
+
+      // ダイアログ非表示時のイベント
+      $('#markerModal').on('hide.bs.modal', function (e) {
+        $log.debug( 'Hide dialog.' );
         if ($scope.markers.dialogTarget !== undefined) {
           $scope.markers.dialogTarget.setAnimation(null);
           $scope.markers.dialogTarget = undefined;
--- app/scripts/services/routemarker.js
+++ app/scripts/services/routemarker.js
@@ -10,33 +10,89 @@
 angular.module('angsmpApp')
   .service('RouteMarker', ['uiGmapLogger', function ($log) {
     // AngularJS will instantiate a singleton by calling "new" on this function
-    $log.currentLevel = $log.LEVELS.debug;
-    this.models = [];
-    this.modelsByGenId = [];
-    this.nextid = 1;
-    this.addMarker = function (lat, lng) {
-      var genid = 'sample-' + (this.nextid++);
-      var genMarker = {
+    $log.currentLevel = $log.LEVELS.debug;  // ログレベルの設定
+    this.models = [];         // マーカー格納エリア
+    this.modelsByGenId = [];  // IDからの参照用マーカー格納エリア
+    this.nextid = 1;          // ID値
+
+    // マーカーID生成
+    this.genId = function () {
+      return 'sample-' + (this.nextid++);
+    };
+
+    // マーカー生成（位置情報のみ）
+    this.genMarker = function (genid, lat, lng) {
+      var genmarker = {
         id: genid,
         latitude: lat,
         longitude: lng,
         title: genid
       };
+      return genmarker;
+    };
+
+    // マーカー追加
+    this.addMarker = function (lat, lng) {
+      var genid = this.genId();
+      var genmarker = this.genMarker(genid, lat, lng);
       $log.debug('create marker:' + genid);
-      this.models.push(genMarker);
-      this.modelsByGenId[genid] = genMarker;
+      this.models.push(genmarker);
+      this.modelsByGenId[genid] = genmarker;
       $log.debug(this.models);
     };
+
+    // マーカー挿入
+    this.insMarker = function (id, lat, lng, prev) {
+      var genid;
+      var genmarker;
+
+      // 挿入位置捜索
+      for (var i = 0; i < this.models.length; i++) {
+        if (this.models[i].id === id) {
+          // 挿入位置発見、マーカー作成
+          genid = this.genId();
+          genmarker = this.genMarker(genid, lat, lng);
+          if (prev === true) {
+            // 直前に挿入の場合
+            this.modelsByGenId[genid] = genmarker;
+            this.models.splice(i, 0, genmarker);
+          }
+          else {
+            // 直後に挿入の場合
+            if (i === (this.models.length - 1)) {
+              // 最終位置なら末尾に追加
+              this.modelsByGenId[genid] = genmarker;
+              this.models.push(genmarker);
+            }
+            else {
+              // 最終位置でなければ直後のマーカーの直前に挿入
+              this.modelsByGenId[genid] = genmarker;
+              this.models.splice(i + 1, 0, genmarker);
+            }
+          }
+          break;
+        }
+      }
+    };
+
+    // マーカー格納エリア返却
     this.getModel = function () {
       return this.models;
-    }
+    };
+
+    // マーカー情報取得
     this.getMarker = function (id) {
       return this.modelsByGenId[id];
     };
+
+    // メーカー情報削除
     this.deleteMarker = function (id) {
+      // ID参照用配列から削除
       delete this.modelsByGenId[id];
+      // 削除対象の位置捜索
       for (var i = 0; i < this.models.length; i++) {
         if (this.models[i].id === id) {
+          // 削除対象発見、配列から削除
           this.models.splice(i, 1);
           break;
         }
